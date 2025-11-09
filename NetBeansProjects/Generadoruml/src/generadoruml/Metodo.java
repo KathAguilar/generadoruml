0826f2aa30ce59245e9019d07c50dd1742f55abc
kathe <kathe@katherin>
2025-11-09 02:23:22 -0500

    Estructura

---
+++ NetBeansProjects/Generadoruml/src/generadoruml/Metodo.java
@@ -0,0 +1,18 @@
+
+        package generadoruml;
+
+        class Metodo {
+            private final String nombre;
+            private final String tipodato;
+
+        public Metodo(String nombre, String tipodato){
+            this.nombre = nombre; 
+            this.tipodato = tipodato;
+        }
+        public String getNombre() {
+            return nombre;
+        }
+        public String getTipoDato() {
+            return tipodato;
+        }
+        }
