0dce683516e1638a96ed43f049e0b951d04c4b6b
KathAguilar <katherin_joh.aguilar@uao.edu.co>
2025-11-11 11:46:58 -0500

    Refactor organizarUML method to include file handling
    uml

--- NetBeansProjects/Generadoruml/src/generadoruml/GeneradorUML.java
+++ NetBeansProjects/Generadoruml/src/generadoruml/GeneradorUML.java
@@ -1,19 +1,41 @@
 
 package generadoruml;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class GeneradorUML {
- public void organizarUML(Modelo modelo){
-    String uml = "@startuml\n"; // salto entre comillas para cadena
-  for (Clase nombreclases  : modelo.getClases()) {
-            String simbolo = obtenerSimbolo(nombreclases.getVisibilidad());
-            uml += simbolo + " class " + nombreclases.getNombre() + " {\n"; // llave abierta para datos de cada clase
-        
-        
-    
 
-}
- 
- }   
+    public void organizarUML(Modelo modelo) {
+        OrganizadorArchivo archivo = new OrganizadorArchivo();
+        archivo.crearArchivo("uml");
+        String uml = "@startuml\n";
+        
+
+        for (Clase c : modelo.getClases()) {
+            uml += "class " + c.getNombre() + " {\n";
+            for (Atributo a : c.getAtributos()) {
+                uml += " " + a.getVisibilidad() + " " + a.getNombre() + " : " + a.getTipo() + "\n";
+            }
+            for (Metodo m : c.getMetodos()) {
+                uml += " " + m.getVisibilidad() + " " + m.getNombre() + "() : " + m.getTipo() + "\n";
+            }
+            uml += "}\n\n";
+        }
+
+        for (Relacion r : modelo.getRelaciones()) {
+            String flecha = "-->";
+            if (r.getTipo().equalsIgnoreCase("herencia")) {
+                flecha = "<|--";
+            } else if (r.getTipo().equalsIgnoreCase("asociacion")) {
+                flecha = "--";
+            } else if (r.getTipo().equalsIgnoreCase("dependencia")) {
+                flecha = "..>";
+            } else if (r.getTipo().equalsIgnoreCase("implementacion")) {
+                flecha = "<|..";
+            }
+            uml += r.getOrigen() + " " + flecha + " " + r.getDestino() + " : " + r.getTipo() + "\n";
+        }
+
+        uml += "@enduml";
+        archivo.guardarArchivo("uml.txt", uml);
+        System.out.println("UML.txt generado correctamente.");
+    }
 }
